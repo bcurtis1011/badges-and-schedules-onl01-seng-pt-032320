891675347ff83863cb4c38cd498fafec4eda531e
determined-constant-1339 <curtis.brodie1011@yahoo.com>
2020-04-05 20:49:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(array)
+  array.collect do |attendee|
+    badge_maker(attendee)
+  end
+end
+
+def assign_rooms(attendees)
+  attendees.map.with_index do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def assign_rooms(array)
+  array.each_with_index do |assigned, index|
+    array[array.index(assigned)] = "Hello, #{assigned}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(attendees)
+  badges = batch_badge_creator(attendees)
+  badges.each do |badge|
+    puts badge
+  end
+  batch_badge_creator(attendees).each do |attendee|
+    puts attendee
+  end
+end
